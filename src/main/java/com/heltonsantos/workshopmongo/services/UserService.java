b6b9b90d7dae2e703bb332aff020e382e1d074eb
Helton Santos <heltonsantosca82@gmail.com>
2021-12-15 22:07:55 -0300

    User Inserction with Post

--- src/main/java/com/heltonsantos/workshopmongo/services/UserService.java
+++ src/main/java/com/heltonsantos/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.heltonsantos.workshopmongo.domain.User;
+import com.heltonsantos.workshopmongo.dto.UserDTO;
 import com.heltonsantos.workshopmongo.repository.UserRepository;
 import com.heltonsantos.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,14 +16,22 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
-	return repo.findAll();	
-		
+
+	public List<User> findAll() {
+		return repo.findAll();
+
 	}
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
-		
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
