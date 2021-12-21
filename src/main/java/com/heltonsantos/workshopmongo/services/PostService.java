c4def99e3796b909140d8faf0b8957dc723122a9
Helton Santos <heltonsantosca82@gmail.com>
2021-12-20 22:22:54 -0300

    Acquiring a post by id

---
+++ src/main/java/com/heltonsantos/workshopmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.heltonsantos.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.heltonsantos.workshopmongo.domain.Post;
+import com.heltonsantos.workshopmongo.repository.PostRepository;
+import com.heltonsantos.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+//	public List<Post> findByTitle(String text) {
+//		return repo.searchTitle(text);
+//	}
+//	
+//	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+//		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+//		return repo.fullSearch(text, minDate, maxDate);
+//	}
+}
