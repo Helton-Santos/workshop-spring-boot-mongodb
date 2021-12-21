4d4c2f3657eb45d5fb0f197911b70408a73aac00
Helton Santos <heltonsantosca82@gmail.com>
2021-12-20 22:05:08 -0300

    Endpoint for return user posts

--- src/main/java/com/heltonsantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/heltonsantos/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.heltonsantos.workshopmongo.domain.Post;
 import com.heltonsantos.workshopmongo.domain.User;
 import com.heltonsantos.workshopmongo.dto.UserDTO;
 import com.heltonsantos.workshopmongo.services.UserService;
@@ -61,5 +62,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value= "/(id)/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
